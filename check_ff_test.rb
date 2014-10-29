ba3802219ac419b187e88375420b12877d741812
Justin Zimmerman <justin.zimmerman@sendgrid.com>
2014-10-28 19:38:34 -0600

    Initial checkin

---
+++ check_ff_test.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'json'
+require 'httparty'
+
+BASE_URL="http://stlb-001.sjc1.sendgrid.net:8082/api/feature_toggle/check.json"
+flags = ["send_campaign","LIST_UPLOAD_THREADING","query_locks","recipient_lists"]
+def check_flag(fname)
+	url = "#{BASE_URL}?feature_name=#{fname}&app_name=nlvx"
+	json_str = HTTParty.get(url).body
+	flag_status = JSON.parse(json_str)
+    return flag_status["enabled"]
+end
+
+flags.each do |f|
+	print "#{f}:  "
+	puts check_flag(f)
+end
