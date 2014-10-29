aea406bf7dd6b619bab76f714cd68afebb6a727f
Justin Zimmerman <justin.zimmerman@sendgrid.com>
2014-10-28 19:40:24 -0600

    Added campaign_pause_play flag

--- check_ff_test.rb
+++ check_ff_test.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'httparty'
 
 BASE_URL="http://stlb-001.sjc1.sendgrid.net:8082/api/feature_toggle/check.json"
-flags = ["send_campaign","LIST_UPLOAD_THREADING","query_locks","recipient_lists"]
+flags = ["send_campaign","LIST_UPLOAD_THREADING","query_locks","recipient_lists","campaign_pause_play"]
 def check_flag(fname)
 	url = "#{BASE_URL}?feature_name=#{fname}&app_name=nlvx"
 	json_str = HTTParty.get(url).body
